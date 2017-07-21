071755f130bd9c4c1641e5a06dce82024a95846a
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-07-20 23:33:10 -0600

    Salto de linea eliminado

--- test-library.php
+++ test-library.php
@@ -4,6 +4,6 @@ require_once __DIR__ . "/vendor/autoload.php";
 $database = (new MongoDB\Client)->test;
 
 $cursor = $database->command(['ping' => 1]);
-echo "<br>";
+
 var_dump($cursor->toArray()[0]);
 ?>
