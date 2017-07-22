2ae369ef3d30f3aa85772fb875288c398ef21d17
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-07-21 19:26:26 -0600

    Prueba

--- test-library.php
+++ test-library.php
@@ -1,9 +1,29 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 
-$database = (new MongoDB\Client)->test;
+$client=new MongoDB\Client;
+
+$database = $client ->test;
 
 $cursor = $database->command(['ping' => 1]);
 
 var_dump($cursor->toArray()[0]);
+
+echo "<br><br>";
+
+/*var_dump($database); 
+
+echo "<br><br>";
+foreach ((new MongoDB\Client)->listDatabases() as $databaseInfo) {
+    var_dump($databaseInfo);
+    //echo $databaseInfo["name"]."<br>";
+    
+}*/
+
+$collection = $client->test->beers;
+
+$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
+
+echo "Inserted with Object ID '{$result->getInsertedId()}'";
+
 ?>
