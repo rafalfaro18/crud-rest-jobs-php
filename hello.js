bb5e897b7bfbaea68cc723764673a2707ffd066b
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-07-23 03:42:02 -0600

    CRUD de Jobs funcional

--- hello.js
+++ hello.js
@@ -61,6 +61,54 @@ app.factory("services", ['$http', function($http) {
         });
     };
 
+    ///////////////////////////
+
+    obj.getJobs = function(){
+        var dataObj={
+        action : 'LEEPOS',
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config);
+    };
+
+    obj.getJob = function(customerID){
+        var dataObj={
+        action : 'LEEUNOPOS',
+        id : customerID,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config);
+    };
+
+    obj.delJobs = function(newid){
+        var dataObj={
+        action: 'DELPOS',
+        id : newid,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config);
+    };
+
+    obj.insertJob = function (newnombre, newdescripcion) {
+        var dataObj={
+        action: 'ADDPOS',
+        nombre : newnombre,
+        descripcion: newdescripcion,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config).then(function (results) {
+            return results;
+        });
+    };
+
+    obj.updateJob = function (newid,newnombre, newdescripcion) {
+        var dataObj={
+        action: 'UPDPOS',
+        id: newid,
+        descripcion: newdescripcion,
+        nombre : newnombre,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config).then(function (status) {
+            return status.data;
+        });
+    };
+
     return obj;   
 }]);
 
@@ -68,22 +116,18 @@ app.controller('listCtrl', function ($scope, services) {
     services.getCustomers().then(function(data){
         $scope.customers = data.data.response;
     });
-
 });
 
 app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams, services, customer) {
     
     var customerID = $routeParams.customerID;
-    console.log(customerID);
        $rootScope.title = (customerID == "0") ? 'Add Customer' : 'Edit Customer';
         $scope.buttonText = (customerID == "0") ? 'Add New Customer' :  'Update Customer';
       if(customerID!="0"){
           var original = customer.data.response;
-          console.log(customer.data);
           original._id = customerID;
           $scope.customer = angular.copy(original);
           $scope.customer._id = customerID;
-          console.log($scope.customer);
       }else{
          $scope.customer=null;
       }
@@ -109,7 +153,48 @@ app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams
     };
 });
 
+///////////////////////////
 
+app.controller('listCtrlPos', function ($scope, services) {
+    services.getJobs().then(function(data){
+        $scope.customers = data.data.response;
+    });
+});
+
+app.controller('editCtrlPos', function ($scope, $rootScope, $location, $routeParams, services, customer) {
+    
+    var customerID = $routeParams.customerID;
+       $rootScope.title = (customerID == "0") ? 'Add Position' : 'Edit Position';
+        $scope.buttonText = (customerID == "0") ? 'Add New Position' :  'Update Position';
+      if(customerID!="0"){
+          var original = customer.data.response;
+          original._id = customerID;
+          $scope.customer = angular.copy(original);
+          $scope.customer._id = customerID;
+      }else{
+         $scope.customer=null;
+      }
+
+      $scope.isClean = function() {
+        return angular.equals(original, $scope.customer);
+      }
+
+      $scope.deleteJob = function(customer) {
+        $location.path('/');
+        if(confirm("Are you sure to delete job number: "+$scope.customer._id)==true)
+        services.delJobs($scope.customer._id);
+      };
+
+      $scope.saveJob = function(customer) {
+        $location.path('/');
+        if (customerID != "0") {
+            services.updateJob(customer._id, customer.name, customer.description);
+        }
+        else {
+            services.insertJob(customer.name, customer.description);
+        }
+    };
+});
 
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
@@ -129,6 +214,22 @@ app.config(['$routeProvider', function($routeProvider) {
           customer: function(services, $route){
             var customerID = $route.current.params.customerID;
             return services.getCustomer(customerID);
+          }
+        }
+    })
+    .when("/jobs", {
+        title: 'Jobs',
+        templateUrl : "jobs.html",
+        controller: 'listCtrlPos'
+    })
+    .when("/edit-job/:customerID", {
+         title: 'Edit Jobs',
+        templateUrl : "edit-jobs.html",
+        controller: 'editCtrlPos',
+        resolve: {
+          customer: function(services, $route){
+            var customerID = $route.current.params.customerID;
+            return services.getJob(customerID);
           }
         }
     })
