46e84a9f9a6ada8f0b3d73870fae629537cb81c6
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-07-22 18:30:17 -0600

    Codigo para Obtener Un Candidato

--- hello.js
+++ hello.js
@@ -18,11 +18,25 @@ app.factory("services", ['$http', function($http) {
     obj.getCustomers = function(){
         var dataObj={
         action : 'LEE',
-        nombre: 'Rafa',
-
         };
         return $http.post('./api.php', Object.toparams(dataObj), config);
     }
+
+    obj.getCustomer = function(customerID){
+        var dataObj={
+        action : 'LEEUNO',
+        id : customerID,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config);
+    }
+
+    obj.delCustomers = function(newid){
+        var dataObj={
+        id : newid,
+        };
+        return $http.post('./api.php', Object.toparams(dataObj), config);
+    }
+
     return obj;   
 }]);
 
@@ -30,7 +44,28 @@ app.controller('listCtrl', function ($scope, services) {
     services.getCustomers().then(function(data){
         $scope.customers = data.data.response;
     });
+
 });
+
+app.controller('editCtrl', function ($scope, $rootScope, $location, $routeParams, services, customer) {
+    var customerID = ($routeParams.customerID) ? parseInt($routeParams.customerID) : 0;
+   
+      var original = customer.data;
+      original._id = customerID;
+      $scope.customer = angular.copy(original);
+      $scope.customer._id = customerID;
+
+      $scope.isClean = function() {
+        return angular.equals(original, $scope.customer);
+      }
+
+      $scope.deleteCustomer = function(customer) {
+        $location.path('/');
+        if(confirm("Are you sure to delete customer number: "+$scope.customer._id)==true)
+        services.deleteCustomer(customer.customerNumber);
+      };
+});
+
 
 
 app.config(['$routeProvider', function($routeProvider) {
@@ -41,6 +76,16 @@ app.config(['$routeProvider', function($routeProvider) {
     .when("/candidates", {
         templateUrl : "candidates.html",
         controller: 'listCtrl'
+    })
+    .when("/edit-customer/:customerID", {
+        templateUrl : "edit-candidate.html",
+        controller: 'editCtrl',
+        resolve: {
+          customer: function(services, $route){
+            var customerID = $route.current.params.customerID;
+            return services.getCustomer(customerID);/*Falta*/
+          }
+        }
     })
 }]);
 
