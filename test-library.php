5d30c490b6c36e26e57372572b8364570cedfab2
Rafael Alfaro <rafalfaro_18@outlook.com>
2017-07-24 01:01:58 -0600

    test-library.php restaurado

--- test-library.php
+++ test-library.php
@@ -1,56 +1,12 @@
 <?php
-//require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/vendor/autoload.php";
 
-//$client=new MongoDB\Client;
+$client=new MongoDB\Client;
 
-//$database = $client ->test;
+$database = $client ->test;
 
-//$cursor = $database->command(['ping' => 1]);
+$cursor = $database->command(['ping' => 1]);
 
-/*var_dump($cursor->toArray()[0]);
-
-echo "<br><br>";*/
-
-/*var_dump($database); 
-
-echo "<br><br>";
-foreach ((new MongoDB\Client)->listDatabases() as $databaseInfo) {
-    var_dump($databaseInfo);
-    //echo $databaseInfo["name"]."<br>";
-    
-}*/
-
-/*$collection = $client->test->beers;
-
-$result = $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
-
-echo "Inserted with Object ID '{$result->getInsertedId()}'";*/
-
-/*$collection = (new MongoDB\Client)->test->candidate;
-$result = $collection->find();
-$arr['msg']='Candidatos Leidos';
-foreach ($result as $document) {
-   $data[]= $document;
-}
-$arr['response'] = $data;
-echo json_encode($arr);*/
-
-/*$collection = (new MongoDB\Client)->test->candidate;
-$result = $collection->find(array(),array('_id'=>0));
-$arr['msg']='Candidatos Leidos';
-foreach ($result as $document) {
-   $data[]= $document;
-}
-
-var_dump(json_encode($data));*/
-
-$postdata = file_get_contents("php://input");
-$obj = $postdata;
-var_dump($obj);
-
-echo "<br><br>";
-
-var_dump($_POST);
 
 
 ?>
